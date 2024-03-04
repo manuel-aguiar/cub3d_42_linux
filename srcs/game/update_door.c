9a6bc7575a83ae3218cc708f992489535b4e8261
Manuel Aguiar <113345121+manuel-aguiar@users.noreply.github.com>
2024-03-04 16:08:56 +0000

    commiting

--- srcs/game/update_door.c
+++ srcs/game/update_door.c
@@ -12,40 +12,45 @@
 
 #include "game.h"
 
-void		update_door(t_game *game, t_sprite *sprite)
+static inline void	close_door(t_game *game, t_sprite *sprite, t_door *door)
 {
-	t_door *door;
-	float dist;
+	if (door->orient == NS)
+		sprite->posi.x = float_clamp(sprite->posi.x + door->move_sense \
+			* game->player.clock->elapsed, door->base_position.x - 1.0f, \
+			door->base_position.x);
+	else
+		sprite->posi.y = float_clamp(sprite->posi.y + door->move_sense \
+			* game->player.clock->elapsed, door->base_position.y - 1.0f, \
+			door->base_position.y);
+}
+
+static inline void	open_door(t_game *game, t_sprite *sprite, t_door *door)
+{
+	if (door->orient == NS)
+		sprite->posi.x = float_clamp(sprite->posi.x - door->move_sense \
+			* game->player.clock->elapsed, door->base_position.x - 1.0f, \
+			door->base_position.x);
+	else
+		sprite->posi.y = float_clamp(sprite->posi.y - door->move_sense \
+			* game->player.clock->elapsed, door->base_position.y - 1.0f, \
+			door->base_position.y);
+}
+
+void	update_door(t_game *game, t_sprite *sprite)
+{
+	t_door	*door;
+	float	dist;
 
 	door = (t_door *)sprite->data;
 	dist = fpow_2(game->player.map_posi.x - door->base_position.x) \
-		 + fpow_2(game->player.map_posi.y - door->base_position.y);
+		+ fpow_2(game->player.map_posi.y - door->base_position.y);
 	door->state = DOOR_CLOSED;
 	if (dist < door->dist_sense)
-	{
-		if (door->orient == NS)
-			sprite->posi.x = float_clamp(sprite->posi.x - door->move_sense \
-				* game->player.clock->elapsed, door->base_position.x - 1.0f, \
-				door->base_position.x);
-		else
-			sprite->posi.y = float_clamp(sprite->posi.y - door->move_sense \
-				* game->player.clock->elapsed, door->base_position.y - 1.0f, \
-				door->base_position.y);
-	}
+		open_door(game, sprite, door);
 	else
-	{
-		if (door->orient == NS)
-			sprite->posi.x = float_clamp(sprite->posi.x + door->move_sense \
-				* game->player.clock->elapsed, door->base_position.x - 1.0f, \
-				door->base_position.x);
-		else
-			sprite->posi.y = float_clamp(sprite->posi.y + door->move_sense \
-				* game->player.clock->elapsed, door->base_position.y - 1.0f, \
-				door->base_position.y);
-	}
+		close_door(game, sprite, door);
 	if (door->orient == NS && door->base_position.x - sprite->posi.x == 1.0f)
 		door->state = DOOR_OPEN;
 	if (door->orient == WE && door->base_position.y - sprite->posi.y == 1.0f)
 		door->state = DOOR_OPEN;
-		
-} 
+}
