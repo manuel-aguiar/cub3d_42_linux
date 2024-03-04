94680ceebfa5b788845409ed17415ba9a0ed7a7f
Manuel Aguiar <113345121+manuel-aguiar@users.noreply.github.com>
2024-03-04 15:25:33 +0000

    commiting

--- srcs/main.c
+++ srcs/main.c
@@ -12,26 +12,13 @@
 
 #include "game.h"
 
-void    endianess_test(void)
-{
-    char    arr[10];
-    char    *ptr;
-    
-    ptr = arr;
-    *(int *)ptr = 1;
-    if (ptr[0] == 1)
-        printf("little endian\n");
-    else
-        printf("big endian\n");
-    
-}
-
 int main(int ac, char **av)
 {
-	t_game	game;	
+	t_game	game;
 
 	if (ac != 2)
-		return (0 & ft_printf_fd(STDERR_FILENO, "cub3d: invalid number of arguments\n"));
+		return (0 & ft_printf_fd(STDERR_FILENO, \
+			"cub3d: invalid number of arguments\n"));
 	if (!game_start(&game, av[1]))
 		return (0);
 	rendering_loop(&game);
