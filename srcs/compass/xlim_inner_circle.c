1ae7e0455b71aa3899e75a85d16544c8a932ef08
Manuel Aguiar <113345121+manuel-aguiar@users.noreply.github.com>
2024-03-06 14:07:32 +0000

    commiting

--- srcs/compass/xlim_inner_circle.c
+++ srcs/compass/xlim_inner_circle.c
@@ -35,7 +35,7 @@ void	render_inner_circle(t_win *win, t_compass *comp)
 }
 */
 
-void	fill_lookup_table(t_compass *comp, t_circle *inner, int x, int y)
+static void	fill_lookup_table(t_compass *comp, t_circle *inner, int x, int y)
 {
 	comp->circle_x_lim[y + inner->radius].min \
 			= ft_min(comp->circle_x_lim[y + inner->radius].min, x);
@@ -43,7 +43,7 @@ void	fill_lookup_table(t_compass *comp, t_circle *inner, int x, int y)
 			= ft_max(comp->circle_x_lim[y + inner->radius].max, x);
 }
 
-void	eight_point_fill(t_compass *comp, t_circle *inner, int x, int y)
+static inline void	eight_point_fill(t_compass *comp, t_circle *inner, int x, int y)
 {
 	fill_lookup_table(comp, inner, x, y);
 	fill_lookup_table(comp, inner, -x, y);
