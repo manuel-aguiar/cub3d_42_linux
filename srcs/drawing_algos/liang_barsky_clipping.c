ac871fea3a6b3b7e81b69a190ee73c3b8e697009
Manuel Aguiar <113345121+manuel-aguiar@users.noreply.github.com>
2024-03-07 10:56:37 +0000

    commiting

--- srcs/drawing_algos/liang_barsky_clipping.c
+++ srcs/drawing_algos/liang_barsky_clipping.c
@@ -12,33 +12,15 @@
 
 #include "drawing_algos.h"
 
-static void	lb_safety(t_pixel low_bot, t_pixel hi_top, \
-	t_pixel *start, t_pixel *end)
-{
-	if (start->x > hi_top.x - 1)
-		start->x = hi_top.x - 1;
-	if (start->x < low_bot.x)
-		start->x = low_bot.x;
-	if (start->y > hi_top.y - 1)
-		start->y = hi_top.y - 1;
-	if (start->y < low_bot.y)
-		start->y = low_bot.y;
-	if (end->x > hi_top.x - 1)
-		end->x = hi_top.x - 1;
-	if (end->x < low_bot.x)
-		end->x = low_bot.x;
-	if (end->y > hi_top.y - 1)
-		end->y = hi_top.y - 1;
-	if (end->y < low_bot.y)
-		end->y = low_bot.y;
-}
+float	lb_maxi(float arr[], int n);
+float	lb_mini(float arr[], int n);
 
 static void	lb_ratios1(t_lbclip *lb)
 {
 	if (lb->p1 != 0)
 	{
-		lb->r1 = (float)lb->q1 / (float)lb->p1;
-		lb->r2 = (float)lb->q2 / (float)lb->p2;
+		lb->r1 = lb->q1 / lb->p1;
+		lb->r2 = lb->q2 / lb->p2;
 		if (lb->p1 < 0)
 		{
 			lb->negarr[lb->negind++] = lb->r1;
@@ -56,8 +38,8 @@ static void	lb_ratios2(t_lbclip *lb)
 {
 	if (lb->p3 != 0)
 	{
-		lb->r3 = (float)lb->q3 / (float)lb->p3;
-		lb->r4 = (float)lb->q4 / (float)lb->p4;
+		lb->r3 = lb->q3 / lb->p3;
+		lb->r4 = lb->q4 / lb->p4;
 		if (lb->p3 < 0)
 		{
 			lb->negarr[lb->negind++] = lb->r3;
@@ -71,17 +53,16 @@ static void	lb_ratios2(t_lbclip *lb)
 	}
 }
 
-static int	lb_setup(t_pixel low_bot, t_pixel hi_top, \
-	t_lbclip *lb, t_pixel *start, t_pixel *end)
+static int	lb_setup(t_lbclip *lb)
 {
-	lb->p1 = -(end->x - start->x);
+	lb->p1 = -(lb->line[1].x - lb->line[0].x);
 	lb->p2 = -lb->p1;
-	lb->p3 = -(end->y - start->y);
+	lb->p3 = -(lb->line[1].y - lb->line[0].y);
 	lb->p4 = -lb->p3;
-	lb->q1 = start->x - low_bot.x;
-	lb->q2 = hi_top.x - 1 - start->x;
-	lb->q3 = start->y - low_bot.y;
-	lb->q4 = hi_top.y - 1 - start->y;
+	lb->q1 = lb->line[0].x - lb->box[0].x;
+	lb->q2 = lb->box[1].x - lb->line[0].x;
+	lb->q3 = lb->line[0].y - lb->box[0].y;
+	lb->q4 = lb->box[1].y - lb->line[0].y;
 	lb->posind = 1;
 	lb->negind = 1;
 	lb->posarr[0] = 1;
@@ -92,12 +73,15 @@ static int	lb_setup(t_pixel low_bot, t_pixel hi_top, \
 	return (1);
 }
 
-int	liang_barsky_clipper(t_pixel low_bot, t_pixel hi_top, \
-	t_pixel start, t_pixel end, t_pixel new[])
+int	liang_barsky_hit(t_pixel win[2], t_pixel draw[2], t_pixel res[2])
 {
 	t_lbclip	lb;
 
-	if (!lb_setup(low_bot, hi_top, &lb, &start, &end))
+	lb.box[0] = win[0];
+	lb.box[1] = win[1];
+	lb.line[0] = draw[0];
+	lb.line[1] = draw[1];
+	if (!lb_setup(&lb))
 		return (0);
 	lb_ratios1(&lb);
 	lb_ratios2(&lb);
@@ -105,17 +89,15 @@ int	liang_barsky_clipper(t_pixel low_bot, t_pixel hi_top, \
 	lb.rn2 = lb_mini(lb.posarr, lb.posind);
 	if (lb.rn1 > lb.rn2)
 		return (0);
-	lb.xn1 = start.x + lb.p2 * lb.rn1;
-	lb.yn1 = start.y + lb.p4 * lb.rn1;
-	lb.xn2 = start.x + lb.p2 * lb.rn2;
-	lb.yn2 = start.y + lb.p4 * lb.rn2;
-	start.x = lb.xn1;
-	start.y = lb.yn1;
-	end.x = lb.xn2;
-	end.y = lb.yn2;
-	lb_safety(low_bot, hi_top, &start, &end);
-	new[0] = start;
-	new[1] = end;
+	lb.xn1 = lb.line[0].x + lb.p2 * lb.rn1;
+	lb.yn1 = lb.line[0].y + lb.p4 * lb.rn1;
+	lb.xn2 = lb.line[0].x + lb.p2 * lb.rn2;
+	lb.yn2 = lb.line[0].y + lb.p4 * lb.rn2;
+	lb.line[0].x = lb.xn1;
+	lb.line[0].y = lb.yn1;
+	lb.line[1].x = lb.xn2;
+	lb.line[1].y = lb.yn2;
+	res[0] = lb.line[0];
+	res[1] = lb.line[1];
 	return (1);
 }
-
