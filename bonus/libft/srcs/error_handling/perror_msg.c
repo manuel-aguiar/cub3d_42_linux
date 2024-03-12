c7931a6b51c796eba70bce235e5a844a28d33b8e
Carla Joao Meneses Ramalho <cjoao-me@c2r4s5.42lisboa.com>
2024-03-12 11:50:10 +0000

    commiting

--- bonus/libft/srcs/error_handling/perror_msg.c
+++ bonus/libft/srcs/error_handling/perror_msg.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   perror_msg.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
+/*   By: cjoao-me <cjoao-me@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/19 16:57:37 by codespace         #+#    #+#             */
-/*   Updated: 2023/09/19 16:59:34 by codespace        ###   ########.fr       */
+/*   Updated: 2024/03/12 11:48:00 by cjoao-me         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,20 @@
 
 int	error_msg_int(char *text, int print_to_this_fd, int number_to_return)
 {
+	ft_printf_fd(print_to_this_fd, "%s", "Error\n");
 	ft_printf_fd(print_to_this_fd, "%s", text);
 	return (number_to_return);
 }
 
 void	*error_msg_ptr(char *text, int print_to_this_fd, void *ptr_to_return)
 {
+	ft_printf_fd(print_to_this_fd, "%s", "Error\n");
 	ft_printf_fd(print_to_this_fd, "%s", text);
 	return (ptr_to_return);
 }
 
 void	error_msg_void(char *text, int print_to_this_fd)
 {
+	ft_printf_fd(print_to_this_fd, "%s", "Error\n");
 	ft_printf_fd(print_to_this_fd, "%s", text);
 }
