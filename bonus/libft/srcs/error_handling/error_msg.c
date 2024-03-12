dff8248e0f44c0eaa02e52f22f4411fc980d1ff9
Manuel Aguiar <113345121+manuel-aguiar@users.noreply.github.com>
2024-03-12 12:36:52 +0000

    commiting

--- bonus/libft/srcs/error_handling/error_msg.c
+++ bonus/libft/srcs/error_handling/error_msg.c
@@ -1,31 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   error_msg.c                                        :+:      :+:    :+:   */
+/*   perror_msg.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: cjoao-me <cjoao-me@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/16 12:36:13 by mmaria-d          #+#    #+#             */
-/*   Updated: 2024/03/12 11:29:36 by cjoao-me         ###   ########.fr       */
+/*   Created: 2023/09/19 16:57:37 by codespace         #+#    #+#             */
+/*   Updated: 2024/03/12 11:48:00 by cjoao-me         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "error_handling.h"
 #include "ft_printf.h"
 
-int	perror_msg_int(char *text, int number_to_return)
+int	error_msg_int(char *text, int print_to_this_fd, int number_to_return)
 {
-	perror(text);
+	ft_printf_fd(print_to_this_fd, "%s", "Error\n");
+	ft_printf_fd(print_to_this_fd, "%s", text);
 	return (number_to_return);
 }
 
-void	*perror_msg_ptr(char *text, void *ptr_to_return)
+void	*error_msg_ptr(char *text, int print_to_this_fd, void *ptr_to_return)
 {
-	perror(text);
+	ft_printf_fd(print_to_this_fd, "%s", "Error\n");
+	ft_printf_fd(print_to_this_fd, "%s", text);
 	return (ptr_to_return);
 }
 
-void	perror_msg_void(char *text)
+void	error_msg_void(char *text, int print_to_this_fd)
 {
-	perror(text);
+	ft_printf_fd(print_to_this_fd, "%s", "Error\n");
+	ft_printf_fd(print_to_this_fd, "%s", text);
 }
