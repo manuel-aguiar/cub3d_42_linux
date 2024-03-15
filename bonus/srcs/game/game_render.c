30e29087292ab89b6499cff57b9b6776ffa7e431
Manuel Maria Do Nascimento Aguiar <mmaria-d@c2r1s6.42lisboa.com>
2024-03-15 10:44:57 +0000

    commiting

--- bonus/srcs/game/game_render.c
+++ bonus/srcs/game/game_render.c
@@ -12,7 +12,7 @@
 
 #include "game.h"
 
-void	game_is_lost(t_game *game)
+static inline void	game_is_lost(t_game *game)
 {
 	if (game->cur_time_lost_str >= game->total_time_lost_str)
 		free_game(game);
@@ -20,11 +20,12 @@ void	game_is_lost(t_game *game)
 	{
 		game->cur_time_lost_str += (game->clock.elapsed + 1);
 		window_pause_manager(&game->win, PAUSE_ON, \
-			game->blur_is_on, you_lost_text_string);
+			game->blur_is_on);
+		game->win.string = you_lost_text_string;
 	}
 }
 
-void	render_as_usual(t_game *game)
+static void	render_as_usual(t_game *game)
 {
 	ft_memset(game->win.front_buf.pixels, 0, \
 		game->win.width * game->win.height * game->win.rgb_size);
@@ -42,27 +43,41 @@ void	render_as_usual(t_game *game)
 	}
 }
 
-int	game_render(t_game *game)
+static inline void	apply_game_state(t_game *game)
 {
-	game_actions(game);
-	game->blur_is_on = (game->win.keys >> BIT_BLUR_T) & 1;
 	if (game->is_lost)
 		game_is_lost(game);
 	else if ((game->win.keys >> BIT_PAUSE_T) & 0xff)
+	{
 		window_pause_manager(&game->win, PAUSE_ON, \
-			game->blur_is_on, pause_text_string);
+			game->blur_is_on);
+		game->win.string = pause_text_string;
+	}
 	else if (game->win.blur.elapsed > 0)
+	{
 		window_pause_manager(&game->win, PAUSE_OFF, \
-			game->blur_is_on, pause_text_string);
+			game->blur_is_on);
+		game->win.string = pause_text_string;
+	}
 	else
 		render_as_usual(game);
+}
+
+int	game_render(t_game *game)
+{
+	game->win.string = NULL;
+	game_actions(game);
+	game->blur_is_on = (game->win.keys >> BIT_BLUR_T) & 1;
+	apply_game_state(game);
 	mlx_put_image_to_window(game->win.mlx, game->win.mlx_win, \
 		game->win.front_buf.img, 0, 0);
 	if (game->enemy_count == 0 && game->cur_time_win_str < \
 		game->total_time_win_str)
 	{
 		game->cur_time_win_str += game->clock.elapsed;
-		enemies_defeated_text_string(&game->win);
+		game->win.string = enemies_defeated_text_string;
 	}
+	if (game->win.string)
+		game->win.string(&game->win);
 	return (1);
 }
